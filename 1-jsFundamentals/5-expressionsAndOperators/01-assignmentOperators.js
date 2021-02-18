b837f75eb7f5f717357eb6379037a13b5176e945
Jennifer <garrett.jennifer0622@gmail.com>
2021-02-18 16:38:35 -0500

    Module work

---
+++ 1-jsFundamentals/5-expressionsAndOperators/01-assignmentOperators.js
@@ -0,0 +1,40 @@
+/*
+ASSIGNMENT OPERATORS
+
+5-expressionsAndOperators
+    01-assignmentsOperators.js
+*/
+
+let x = 20;
+
+//addition assignmtn operator
+x += 1; //x = x +1
+console.log(x);
+
+//subtraction assignment operator
+x -= 1; //x = x - 1
+console.log(x);
+
+//multiplication assignment operator
+x *= 2 //x = x* 2
+console.log(x);
+
+//division assignment operator
+x /= 4; //x = x / 4
+console.log(x);
+
+//remainder (modulous) assignment operator
+x %= 2;
+console.log(x);
+
+//modulous operator (what's left over)
+console.log(5%3); // 5/3 -> 1 2/3 -> 2
+console.log(25%7); // 25/7 -> 3 4/7 -> 4
+
+//exponential assignment operator
+x **= 2;
+console.log(x);
+
+let y = 7;
+y **= 3;
+console.log(y);
