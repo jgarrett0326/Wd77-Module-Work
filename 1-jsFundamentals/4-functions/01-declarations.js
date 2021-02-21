009fea8de816749409b7c9b6c1a94415da43c867
Jennifer <garrett.jennifer0622@gmail.com>
2021-02-20 21:15:38 -0500

    new work

--- 1-jsFundamentals/4-functions/01-declarations.js
+++ 1-jsFundamentals/4-functions/01-declarations.js
@@ -32,8 +32,12 @@ console.log(superImportantSentence);
 //item in the input array
 
 let arr = [1, true, {key: 'string'}, [0, false, null], 'heya!'];
-let arr2 = [5,6,7,8,9,10];
+let arr2 = [5,6,7,8.9,10];
 
 function iteratorFunc(inputArr){
-    for (let element of inputArr)
+    for (let element of inputArr){
+        console.log(element);
+    }
 }
+iteratorFunc(arr);
+iteratorFunc(arr2);
